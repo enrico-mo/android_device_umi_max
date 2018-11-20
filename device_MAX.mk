e9eaf3c6e221a3f9cd3537163d892ffac5111cb5
enrico-mo <enrico.caporali.mo@gmail.com>
2018-11-20 06:42:00 +0100

    Added vendor blobs & properties

--- device_MAX.mk
+++ device_MAX.mk
@@ -15,8 +15,6 @@
 # limitations under the License.
 #
 
-include device/umi/MAX/mt6755.mk
-
 # Call heap & hwui memory config
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxxhdpi-3072-dalvik-heap.mk)
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxxhdpi-3072-hwui-memory.mk)
@@ -132,7 +130,6 @@ PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/bluetooth/bt_stack.conf.sqc:system/e
 PRODUCT_PACKAGES += Snap
 PRODUCT_PACKAGES += mtkcamera_parameters
 PRODUCT_PROPERTY_OVERRIDES += camera2.portability.force_api=1
-
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/camera/camerasize.xml:system/etc/camerasize.xml
 PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml
 PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
@@ -151,11 +148,130 @@ PRODUCT_PACKAGES += charger
 # Dalvik
 PRODUCT_TAGS += dalvik.gc.type-precise
 
+# Default.prop
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += persist.service.acm.enable=0
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += camera.disable_zsl_mode=1
+
 # DHCPCD
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/dhcpcd/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf
 
+# Display
+PRODUCT_PACKAGES += libion
+
+# Fingerprint
+PRODUCT_PACKAGES += fingerprintd
+#PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
+
+# FMRadio
+PRODUCT_PACKAGES += FMRadio
+
+# Filesystem management tools
+PRODUCT_PACKAGES +=e2fsck
+PRODUCT_PACKAGES +=fibmap.f2fs
+PRODUCT_PACKAGES +=fsck.f2fs
+PRODUCT_PACKAGES +=mkfs.f2fs
+PRODUCT_PACKAGES +=make_ext4fs
+PRODUCT_PACKAGES +=resize2fs
+PRODUCT_PACKAGES +=  setup_fs
+
+# GPS
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+PRODUCT_PACKAGES += libcurl
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/slp_conf:system/etc/slp_conf
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/agps_profiles_conf2.xml:system/etc/agps_profiles_conf2.xml
+PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml
+
+#GPU collection
+PRODUCT_PACKAGES += libvcodec_utility
+PRODUCT_PACKAGES += libvcodec_oal
+PRODUCT_PACKAGES += libh264enc_sa.ca7
+PRODUCT_PACKAGES += libh264enc_sb.ca7
+PRODUCT_PACKAGES += libHEVCdec_sa.ca7.android
+PRODUCT_PACKAGES += libmp4enc_sa.ca7
+PRODUCT_PACKAGES += libmp4enc_xa.ca7
+PRODUCT_PACKAGES += libvc1dec_sa.ca7
+PRODUCT_PACKAGES += libvp8dec_sa.ca7
+PRODUCT_PACKAGES += libvp8enc_sa.ca7
+PRODUCT_PACKAGES += libvp9dec_sa.ca7
+PRODUCT_PACKAGES += audio.primary.default
+PRODUCT_PACKAGES += audio.primary.mt6755
+PRODUCT_PACKAGES += audio_policy.stub
+PRODUCT_PACKAGES += local_time.default
+PRODUCT_PACKAGES += libaudiocustparam
+PRODUCT_PACKAGES += libvcodec_oal
+PRODUCT_PACKAGES += liblic_divx
+PRODUCT_PACKAGES += liblic_s263
+PRODUCT_PACKAGES += init.factory.rc
+PRODUCT_PACKAGES += libaudio.primary.default
+PRODUCT_PACKAGES += audio_policy.default
+PRODUCT_PACKAGES += audio_policy.mt6755
+PRODUCT_PACKAGES += libaudio.a2dp.default
+PRODUCT_PACKAGES += libMtkVideoTranscoder
+PRODUCT_PACKAGES += libMtkOmxCore
+#PRODUCT_PACKAGES += libI420colorconvert
+#PRODUCT_PACKAGES += libmp4dec_sa.ca9
+#PRODUCT_PACKAGES += libmp4dec_sb.ca9
+#PRODUCT_PACKAGES += libmp4dec_customize
+#PRODUCT_PACKAGES += libvp8dec_xa.ca9
+#PRODUCT_PACKAGES += libmp4enc_xa.ca9
+#PRODUCT_PACKAGES += libmp4enc_xb.ca9
+#PRODUCT_PACKAGES += libh264enc_sa.ca9
+#PRODUCT_PACKAGES += libh264enc_sb.ca9
+#PRODUCT_PACKAGES += libvc1dec_sa.ca9
+#PRODUCT_PACKAGES += libMtkOmxOsalUtils
+
+# Keyboard layout
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/keylayout/mtk-kpd.kl:system/usr/keylayout/mtk-kpd.kl
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/keylayout/ACCDET.kl:system/usr/keylayout/ACCDET.kl
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/keylayout/Generic.kl:system/usr/keylayout/Generic.kl
+
+# Keyhandler
+PRODUCT_PACKAGES +=  com.cyanogenmod.keyhandler
+PRODUCT_SYSTEM_SERVER_JARS += com.cyanogenmod.keyhandler
+# Never dexopt the keyhandler
+$(call add-product-dex-preopt-module-config,com.cyanogenmod.keyhandler,disable)
+
+# Keyswitch
+PRODUCT_PACKAGES += KeySwitch
+
+# Lights
+PRODUCT_PACKAGES += lights.mt6755
+
+# Media
+PRODUCT_COPY_FILES += frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml
+PRODUCT_COPY_FILES += frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml
+PRODUCT_COPY_FILES += frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/media_codecs_mediatek_audio.xml:system/etc/media_codecs_mediatek_audio.xml
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/media_codecs_mediatek_video.xml:system/etc/media_codecs_mediatek_video.xml
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/mtk_omx_core.cfg:system/etc/mtk_omx_core.cfg
+
+# Mediatek platform
+PRODUCT_PACKAGES += libmtk_symbols
+
+# Mtklog config
+ifeq ($(TARGET_BUILD_VARIANT),eng)
+    PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/mtklog/mtklog-config-bsp-eng.prop:system/etc/mtklog-config.prop
+else
+    PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/mtklog/mtklog-config-bsp-user.prop:system/etc/mtklog-config.prop
+endif
+
+# MultiWindow
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += persist.sys.debug.multi_window=true
+
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += device/umi/MAX/overlay
+
+# Power
+PRODUCT_PACKAGES += power.default
+PRODUCT_PACKAGES += power.mt6755
+
+# Radio dependencies
+PRODUCT_PACKAGES += muxreport
+PRODUCT_PACKAGES += terservice
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/spn-conf.xml:system/etc/spn-conf.xml
 
 # Ramdisk (REMOVED: init.nvdata.rc)
 PRODUCT_PACKAGES += enableswap.sh
@@ -175,6 +291,23 @@ PRODUCT_PACKAGES += meta_init.rc
 PRODUCT_PACKAGES += ueventd.mt6755.rc
 PRODUCT_PACKAGES += init.recovery.mt6755.rc
 
+# Remove Packages
+PRODUCT_PACKAGES += RemovePackages
+
+# RIL
+PRODUCT_PROPERTY_OVERRIDES += ro.kernel.android.checkjni=0
+PRODUCT_PROPERTY_OVERRIDES += ro.telephony.ril_class=MT6755
+PRODUCT_PROPERTY_OVERRIDES += ro.telephony.ril.config=fakeiccid
+
+# Sensors
+PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml
+PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml
+PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml
+PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml
+PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml
+PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:system/etc/permissions/android.hardware.sensor.stepcounter.xml
+PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:system/etc/permissions/android.hardware.sensor.stepdetector.xml
+
 #SPM
 PRODUCT_PACKAGES += spm_loader
 PRODUCT_PACKAGES += pcm_deepidle.bin
@@ -187,7 +320,13 @@ PRODUCT_PACKAGES += pcm_vcorefs_hpm.bin
 PRODUCT_PACKAGES += pcm_vcorefs_lpm.bin
 PRODUCT_PACKAGES += pcm_vcorefs_ultra.bin
 
+# Telecomm
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/ecc_list.xml:system/etc/ecc_list.xml
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/spn-conf.xml:system/etc/spn-conf.xml
+
 # Telephony
+SIM_COUNT := 2
+PRODUCT_PROPERTY_OVERRIDES += ro.telephony.sim.count=$(SIM_COUNT)
 PRODUCT_PACKAGES += muxreport
 PRODUCT_PACKAGES += mtkrild
 PRODUCT_PACKAGES += mtk-ril
@@ -202,6 +341,40 @@ PRODUCT_PACKAGES += BSPTelephonyDevTool
 PRODUCT_PACKAGES += ppl_agent
 PRODUCT_COPY_FILES += device/umi/MAX/configs/apns-conf.xml:system/etc/apns-conf.xml
 PRODUCT_COPY_FILES += device/umi/MAX/configs/spn-conf.xml:system/etc/spn-conf.xml
+
+# USB
+PRODUCT_PACKAGES += librs_jni
+PRODUCT_PACKAGES += com.android.future.usb.accessory
+
+# Wide-dhcpv6
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/wide-dhcpv6/dhcp6c.conf:system/etc/wide-dhcpv6/dhcp6c.conf
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/wide-dhcpv6/dhcp6c.script:system/etc/wide-dhcpv6/dhcp6c.script
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/wide-dhcpv6/dhcp6cctlkey:system/etc/wide-dhcpv6/dhcp6cctlkey
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/wide-dhcpv6/dhcp6cDNS.conf:system/etc/wide-dhcpv6/dhcp6cDNS.conf
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/wide-dhcpv6/dhcp6cPD.conf:system/etc/wide-dhcpv6/dhcp6cPD.conf
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/wide-dhcpv6/dhcp6cPD.script:system/etc/wide-dhcpv6/dhcp6cPD.script
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/wide-dhcpv6/dhcp6s.conf:system/etc/wide-dhcpv6/dhcp6s.conf
+
+# Wifi
+PRODUCT_PACKAGES += libwpa_client
+PRODUCT_PACKAGES += lib_driver_cmd_mt66xx
+PRODUCT_PACKAGES += libwifi-hal-mt66xx
+PRODUCT_PACKAGES += hostapd
+PRODUCT_PACKAGES += dhcpcd.conf
+PRODUCT_PACKAGES += wpa_supplicant
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/hostapd/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/hostapd/hostapd.accept:system/etc/hostapd/hostapd.accept
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/hostapd/hostapd.deny:system/etc/hostapd/hostapd.deny
+PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml
+PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml
+PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml
+PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml
+PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
+PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml
+PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:system/etc/permissions/android.hardware.sensor.stepcounter.xml
+PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:system/etc/permissions/android.hardware.sensor.stepdetector.xml
+
+###########################################################################################
 
 # Inherit common configurations from los sources
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
